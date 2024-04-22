13425698359f7fbb9a6b3cd8a23504e7d30dde61
ekov1 <kristian_ekov@abv.bg>
2024-04-22 23:33:36 +0300

    add simple state machine

---
+++ StateDesignPattern/00. StateMachine/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _00._StateMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car();
+            car.TakeAction(Car.Action.Accelerate);
+            PrintState(car);
+            car.TakeAction(Car.Action.Start);
+            PrintState(car);
+            car.TakeAction(Car.Action.Start);
+            PrintState(car);
+            car.TakeAction(Car.Action.Accelerate);
+            PrintState(car);
+            car.TakeAction(Car.Action.Stop);
+            PrintState(car);
+        }
+
+        public static void PrintState(Car car)
+        {
+            Console.WriteLine($"Current state = {car.CurrentState.ToString()}");
+        }
+    }
+}
